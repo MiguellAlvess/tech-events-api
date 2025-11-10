ff2a44b2d805ab8987f742fe84ea5871631f9694
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-11-10 13:18:53 -0300

    chore: add initial project setup

---
+++ src/main/java/com/br/miguelalves/tech_events_api/TechEventsApiApplication.java
@@ -0,0 +1,13 @@
+package com.br.miguelalves.tech_events_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechEventsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechEventsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/miguelalves/tech_events_api/TechEventsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.miguelalves.tech_events_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechEventsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
